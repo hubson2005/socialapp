eea13a9136db3571a9d11cab6beaad1a8c910d84
hubertkouassi <hubertkouassi84@gmail.com>
2026-04-18 11:20:55 +0000

    variables environnement Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://gxguirtpunmiiuxpxlap.supabase.co'
-const SUPABASE_KEY = 'COLLE_TA_PUBLISHABLE_KEY_ICI'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
